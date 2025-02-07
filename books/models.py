f6cdb97b39f7e737c884b015ee6c0cc71c374419
lapuxa23 <esenbektima@gmail.com>
2025-02-07 22:38:33 +0600

    web

--- books/models.py
+++ books/models.py
@@ -23,22 +23,23 @@ class BookModel(models.Model):
         ('CRIME', 'Криминальная литература'),
         ('CLASSIC', 'Классическая литература'),
     )
-    image = models.ImageField(upload_to='books/',verbose_name="download image")
-    title = models.CharField(max_length=100, verbose_name="book title")
-    description = models.TextField(verbose_name="book description", blank=True)
-    price = models.PositiveIntegerField(verbose_name="book price", default=10)
-    created_dat = models.DateField(auto_now_add=True)
-    genre = models.CharField(max_length=20, choices=GENRE_CHOICES,default='HORROR', verbose_name="book genre")
-    gmail = models.URLField(verbose_name="book gmail", blank=True,default='esenbektima@gmail.com')
-    author = models.CharField(max_length=50, verbose_name="book author",default='Timur Tumur')
-    audio = models.URLField(verbose_name="url book",blank=True)
+
+    image = models.ImageField(upload_to='books/', verbose_name="Обложка книги")
+    title = models.CharField(max_length=100, verbose_name="Название книги")
+    description = models.TextField(verbose_name="Описание книги", blank=True)
+    price = models.PositiveIntegerField(verbose_name="Цена", default=10)
+    created_date = models.DateField(auto_now_add=True, verbose_name="Дата создания")
+    genre = models.CharField(max_length=20, choices=GENRE_CHOICES, verbose_name="Жанр книги")
+    email = models.EmailField(verbose_name="Email автора", blank=True, default='esenbektima@gmail.com')
+    author = models.CharField(max_length=50, verbose_name="Автор", default='Timur Tumur')
+    audio_url = models.URLField(verbose_name="Ссылка на аудиокнигу", blank=True)
 
     def __str__(self):
         return self.title
 
     class Meta:
-        verbose_name = 'книга'
-        verbose_name_plural = 'книги'
+        verbose_name = 'Книга'
+        verbose_name_plural = 'Книги'
 
 
 
--- books/urls.py
+++ books/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.book_list_view, name='book_list'),
-    path('film_detail/<int:id>/', views.book_detail_view, name='book_detail'),
-
-    path("about_me/", views.about_me, name='about_me'),
-    path("text_photo/", views.text_photo, name='text_photo'),
-    path("time_now/", views.time_now, name='time_now'),
+    path('book/<int:id>/', views.book_detail_views, name='book_detail'),
+    path('time/', views.time_now, name='time_now'),
+    path('photo/', views.text_photo, name='text_photo'),
+    path('about/', views.about_me, name='about_me'),
 ]
+
